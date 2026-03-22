fd819cced88142f82e8e82b16b1fe7ed56fd2a8c
novice-hero <highero700@gmail.com>
2026-03-22 18:27:10 +0900

    feat: useClickOutside

---
+++ src/shared/hooks/useClickOutside.ts
@@ -0,0 +1,17 @@
+import { RefObject, useEffect } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  ref: RefObject<T>,
+  callback: () => void
+) {
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handler);
+    return () => document.removeEventListener('mousedown', handler);
+  }, [ref, callback]);
+}
